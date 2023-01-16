3cb9536944957703bb4de8f74ba568ecc218bf54
AYOUNG <dkdud7622@ajou.ac.kr>
2023-01-16 13:15:00 +0900

    add tf_example

---
+++ TF_Example.py
@@ -0,0 +1,25 @@
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+
+N,D = 3,4
+
+x = tf.Variable(tf.ones(dtype = tf.float32))
+y = tf.Variable(tf.ones(dtype = tf.float32))
+z = tf.Variable(tf.ones(dtype = tf.float32))
+
+a = x*y
+b = a+z
+c = tf.reduce_sum(b)
+
+grad_x, grad_y, grad_z = tf.gradients(c,[x,y,z])
+
+with tf.Session() as sess :
+    values = {
+        x: np.random.randn(N,D),
+        y: np.random.randn(N,D),
+        z: np.random.randn(N,D),
+    }
+
+    out = sess.run([c,grad_x,grad_y,grad_z],feed_dict=values)
+    c_val, grad_x_val, grad_y_val, grad_z_val = out
